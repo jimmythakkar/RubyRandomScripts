dfad772d0a1883d5454ce3897a6086c0cc11b74c
YourNextLeap <yournextleap@yournextleap-laptop.(none)>
2011-10-04 15:49:31 +0530

    Initial Commit

---
+++ first.rb
@@ -0,0 +1,25 @@
+
+class Person <
+
+  
+  Struct.new(:first_name, :last_name, :city)
+
+end
+
+
+people = Array.new
+
+
+f = File.open("people.csv", "r")
+
+
+f.each_line { |line|
+
+    fields = line.split(',')
+  
+  
+  p = Person.new
+
+    people.push(p)
+}
+
